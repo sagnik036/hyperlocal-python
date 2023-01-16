05e608641269a56e165df5c7ba5b77bcb7224008
sagnik036 <iamsagnikbanik@gmail.com>
2023-01-17 00:55:26 +0530

    python version

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo " BUILD START"
+python3.8.10 -m pip install --upgrade pip
 python3.8.10 -m pip install -r requirements.txt
 python3.8.10 manage.py collectstatic --noinput --clear
 echo " BUILD END"
